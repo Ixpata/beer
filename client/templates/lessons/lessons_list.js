8b4a37d24591429969c586c3acd79fdd93a4775f
Steve Mitchell <ixpata@gmail.com>
2014-12-15 21:08:25 -0500

    grouped lessons by date

--- client/templates/lessons/lessons_list.js
+++ client/templates/lessons/lessons_list.js
@@ -1,5 +1,15 @@
 Template.lessonsList.helpers({
-  lessons: function() {
-    return Lessons.find({}, {sort: {submitted: -1}});
-  }
+	lessons: function() {
+		return Lessons.find({}, {sort: {submitted: -1}});
+	},
+	groupedDates: function() {
+		return _.groupBy(_.pluck(Lessons.find().fetch(), 'dateTime'));
+	},
+	arrayify: function(obj) {
+		result = [];
+		for (var key in obj) {
+			result.push({date:key, value:obj[key]});
+		}
+		return result;
+	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,22 +2,42 @@ if (Lessons.find().count() === 0) {
   Lessons.insert({
     style: 'Swing',
     level: 'Beginner',
-    dateTime: 'Tuesday, December 9, 8 pm',
-    
+    dateTime: 'Monday, December 8, 8 pm',
     name: 'Beginner 8-Count Lindy Hop (Week 3 of 4)',
     url: 'http://thejamcellar.com',
     description: '',
-    location: 'Jam Cellar',
-    
+    location: 'Chevy Chase Ballroom',
     price: 'Free',
     mediaURL: 'http://farm3.staticflickr.com/2003/2358565895_86ef2ea6fc_s.jpg',
     danceAfter: true,
     instructors: ['Hugh Beaumont'],
-    
-    
-
   });
-
+  Lessons.insert({
+    style: 'Swing',
+    level: 'Beginner',
+    dateTime: 'Tuesday, December 9, 8 pm',
+    name: 'Beginner 8-Count Lindy Hop (Week 3 of 4)',
+    url: 'http://thejamcellar.com',
+    description: '',
+    location: 'Jam Cellar',
+    price: 'Free',
+    mediaURL: 'http://farm3.staticflickr.com/2003/2358565895_86ef2ea6fc_s.jpg',
+    danceAfter: true,
+    instructors: ['Hugh Beaumont'],
+  });
+  Lessons.insert({
+    style: 'Swing',
+    level: 'Beginner',
+    dateTime: 'Tuesday, December 9, 8 pm',
+    name: 'Beginner 8-Count Lindy Hop (Week 3 of 4)',
+    url: 'http://thejamcellar.com',
+    description: '',
+    location: 'Jam Cellar',
+    price: 'Free',
+    mediaURL: 'http://farm3.staticflickr.com/2003/2358565895_86ef2ea6fc_s.jpg',
+    danceAfter: true,
+    instructors: ['Hugh Beaumont'],
+  });
   /*Locations.insert({
     name: 'Jam Cellar',
     url: 'http://thejamcellar.com',
