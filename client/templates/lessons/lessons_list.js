5e1bff256456e0b6369961844136042525ec7482
Steve Mitchell <stevemitchell@Steves-MacBook-Air.local>
2015-01-25 19:01:35 -0500

    Filter by checks

--- client/templates/lessons/lessons_list.js
+++ client/templates/lessons/lessons_list.js
@@ -1,15 +1,27 @@
+Template.lessonsList.events({
+  "change .hide-completed input": function (event) {
+	Session.set("hideCompleted", event.target.checked);
+  }
+});
+
 Template.lessonsList.helpers({
-	lessons: function() {
-		return Lessons.find({}, {sort: {submitted: -1}});
-	},
-	groupedDates: function() {
-		return _.groupBy(Lessons.find().fetch(), 'date');
-	},
-	arrayify: function(obj) {
-		result = [];
-		for (var key in obj) {
-			result.push({date:key, value:obj[key]});
-		}
-		return result;
+  lessons: function() {
+	return Lessons.find({});
+  },
+  groupedDates: function() {
+	var lessons;
+	if (Session.get("hideCompleted")) {
+      lessons = Lessons.find({danceAfter: {$ne: false}});
+    } else {
+      lessons = Lessons.find({danceAfter: {$ne: true}});
+    }
+    return _.groupBy(lessons.fetch(), 'date');
+  },
+  arrayify: function(obj) {
+	result = [];
+	for (var key in obj) {
+	  result.push({date:key, value:obj[key]});
 	}
+	return result;
+  }
 });
